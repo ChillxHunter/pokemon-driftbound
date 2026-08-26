511ba4447e6c50fadad22e86bcd3f9eb4bfb17f9
ChillxHunter <hansrobert87@gmail.com>
2026-08-26 16:00:06 +0200

    v104 cache-bust compact mobile battle HUD

--- deploy/apply_v97.py
+++ deploy/apply_v97.py
@@ -6,12 +6,14 @@ p=root/'index.html'
 s=p.read_text(encoding='utf-8')
 
 if 'v97_mobile_battle_fit.css' not in s:
-    s=s.replace('</head>','  <link rel="stylesheet" href="v97_mobile_battle_fit.css?v=mobile-103">\n</head>')
+    s=s.replace('</head>','  <link rel="stylesheet" href="v97_mobile_battle_fit.css?v=mobile-104">\n</head>')
 else:
-    s=s.replace('v97_mobile_battle_fit.css?v=mobile-97','v97_mobile_battle_fit.css?v=mobile-103')
+    import re
+    s=re.sub(r'v97_mobile_battle_fit\.css\?v=mobile-[0-9]+','v97_mobile_battle_fit.css?v=mobile-104',s)
 if 'v97_mobile_battle_fit.js' not in s:
-    s=s.replace('</body>','  <script src="v97_mobile_battle_fit.js?v=mobile-103"></script>\n</body>')
+    s=s.replace('</body>','  <script src="v97_mobile_battle_fit.js?v=mobile-104"></script>\n</body>')
 else:
-    s=s.replace('v97_mobile_battle_fit.js?v=mobile-97','v97_mobile_battle_fit.js?v=mobile-103')
+    import re
+    s=re.sub(r'v97_mobile_battle_fit\.js\?v=mobile-[0-9]+','v97_mobile_battle_fit.js?v=mobile-104',s)
 
 p.write_text(s,encoding='utf-8')
